83533df118606923e5f063a965c887ee5781f250
[Sean Anderson] <[seandersont@gmail.com]>
2019-04-17 22:29:39 -0500

    created a framework for the script

--- restore.ps1
+++ restore.ps1
@@ -1,1 +1,25 @@
-# Start of powershell script 1
+# Requires -version 2
+<#
+.SYNOPSIS
+  This script restores active directory OU's and reconfigures SQL server.
+
+.NOTES
+  Version:        1.0
+  Author:         Sean Anderson
+  Creation Date:  17 April, 2019
+  Purpose/Change: Initial script development
+#>
+
+## PARAMETERS
+
+Param (
+  # Parameters go here
+)
+
+## VARIABLES
+
+## FUNCTIONS
+
+## EXECUTION
+
+
